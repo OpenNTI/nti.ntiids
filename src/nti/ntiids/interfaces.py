c2bc0fa7642e2c7d520ce48eb4b267a602221a39
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-19 16:31:35 +0000

    add logger
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.ntiids/trunk@75000 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/ntiids/interfaces.py
+++ src/nti/ntiids/interfaces.py
@@ -9,6 +9,8 @@ NTIID related interfaces.
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
 from zope import interface
 
 from zope.interface.common.sequence import IMinimalSequence
