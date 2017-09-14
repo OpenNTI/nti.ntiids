b635b68ab3c112c684bbf44846742d0f82b9bc02
Carlos Sanchez <papachoco@gmail.com>
2017-09-14 13:11:59 -0500

    add test

--- src/nti/ntiids/tests/test_oids.py
+++ src/nti/ntiids/tests/test_oids.py
@@ -16,6 +16,7 @@ import unittest
 
 from nti.externalization.externalization import set_external_identifiers
 
+from nti.ntiids.oids import to_external_ntiid_oid
 from nti.ntiids.oids import setExternalIdentifiers
 
 from nti.ntiids.tests import SharedConfiguringTestLayer
@@ -28,6 +29,10 @@ class TestOIDs(unittest.TestCase):
     def test_hookable(self):
         assert_that(set_external_identifiers,
                     has_property('implementation', is_(setExternalIdentifiers)))
-    
-    def test_to_external_ntiid_oid(self):
-        pass
+
+    @fudge.patch('nti.ntiids.oids.toExternalOID')
+    def test_to_external_ntiid_oid(self, mock_teo):
+        mock_teo.is_callable().with_args().returns('0x01:666f6f')
+        ntiid = to_external_ntiid_oid(object())
+        assert_that(ntiid,
+                    is_('tag:nextthought.com,2011-10:zope.security.management.system_user-OID-0x01:666f6'))
