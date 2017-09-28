70d4e34d3eee86d8f1b9983778f3cbe195b24afc
Carlos Sanchez <papachoco@gmail.com>
2017-09-28 08:50:11 -0500

    restore compat module and test

---
+++ src/nti/ntiids/_compat.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from six import text_type
+
+
+def text_(s, encoding='utf-8', err='strict'):
+    """
+    Decode a byte sequence and unicode result
+    """
+    if not isinstance(s, text_type) and s is not None:
+        s = s.decode(encoding, err)
+    return s
+
+
+def bytes_(s, encoding='utf-8', errors='strict'):
+    """
+    If ``s`` is an instance of ``text_type``, return
+    ``s.encode(encoding, errors)``, otherwise return ``s``
+    """
+    if not isinstance(s, bytes) and s is not None:
+        s = s.encode(encoding, errors)
+    return s
