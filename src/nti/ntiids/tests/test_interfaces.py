44b620b6a580095b764d593b17cb77dd6fd0e1fb
Carlos Sanchez <papachoco@gmail.com>
2017-09-29 14:57:38 -0500

    add interface ITuple test

--- src/nti/ntiids/tests/test_interfaces.py
+++ src/nti/ntiids/tests/test_interfaces.py
@@ -8,6 +8,9 @@ from __future__ import absolute_import
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import assert_that
+
 import unittest
 
 from zope.dottedname import resolve as dottedname
@@ -17,3 +20,7 @@ class TestInterfaces(unittest.TestCase):
 
     def test_import_interfaces(self):
         dottedname.resolve('nti.ntiids.interfaces')
+
+    def test_tuple(self):
+        from nti.ntiids.interfaces import ITuple
+        assert_that(ITuple.providedBy(tuple()), is_(True))
