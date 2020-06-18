2d0b4ebfb790cd74ee29a639874ff54815112d9c
Jason Madden <jamadden@gmail.com>
2020-06-18 06:50:15 -0500

    Fix test_generate_ntiid to be agnostic of the local time.

--- src/nti/ntiids/tests/test_common.py
+++ src/nti/ntiids/tests/test_common.py
@@ -7,11 +7,11 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods
 
-from hamcrest import assert_that
-from hamcrest import starts_with
-
 import unittest
 from datetime import datetime
+
+from hamcrest import assert_that
+from hamcrest import starts_with
 
 from nti.ntiids.common import generate_ntiid
 
@@ -23,6 +23,6 @@ class TestCommon(unittest.TestCase):
     layer = SharedConfiguringTestLayer
 
     def test_generate_ntiid(self):
-        ntiid = generate_ntiid(u'FOO', now=datetime.fromtimestamp(1000))
+        ntiid = generate_ntiid(u'FOO', now=datetime.utcfromtimestamp(1000))
         assert_that(ntiid,
-                    starts_with('tag:nextthought.com,2011-10:NTI-FOO-system_19691231181640_000000'))
+                    starts_with('tag:nextthought.com,2011-10:NTI-FOO-system_19700101001640_000000'))
